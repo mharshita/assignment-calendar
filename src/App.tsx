c38d797ab1ff5250146480ce7a1657eaf9a3310f
Harshita Mangla <manglaharshita@gmail.com>
2023-07-22 12:16:06 +0530

    add react-typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Card from "./components/Card";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+};
+
+export default App;
